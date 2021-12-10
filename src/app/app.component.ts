b25b175181a5c6ba702829b08225673d77ad4159
fawzi bazari <fawzi.bazari2@gmail.com>
2021-12-10 15:05:53 +0100

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-register';
+
+  RegisterForm!: FormGroup;
+  //deux façon different pour le email validator 
+  // emailRegx = /^(([^<>+()\[\]\\.,;:\s@"-#$%&=]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/;
+
+constructor(private formBuilder: FormBuilder) {}
+
+ngOnInit() {
+  this.RegisterForm = this.formBuilder.group({
+    name: [null, [Validators.pattern('[A-Z a-z]{1,100}')]],
+    // email: [null, [Validators.required, Validators.pattern(this.emailRegx)]],
+    email: [null, [Validators.email, Validators.required]],
+    password: [null, Validators.required]
+  });
+}
+
+submit() {
+  if (!this.RegisterForm.valid) {
+    return;
+  }
+  console.log(this.RegisterForm.value);
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input'
+import {MatButtonModule} from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +19,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    FormsModule,
+    MatCardModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
